34502b6563b44d302517484fed052780d093fed9
cresence2k24 <cresence@jntugvcev.edu.in>
2025-03-11 16:46:38 +0530

    test-1

--- src/Components/level2.jsx
+++ src/Components/level2.jsx
@@ -177,7 +177,7 @@ int main()
   }, []);
 
   useEffect(() => {
-    const quizDuration = 20 * 60 * 1000; // 20 minutes in milliseconds
+    const quizDuration = 30 * 60 * 1000; // 20 minutes in milliseconds
     // let startTime = localStorage.getItem("startTime");
 
     // if (!startTime) {
