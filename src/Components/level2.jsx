734d5fa6904d9f66818c9c0a0fee77b9f0f050e9
lavetisandeep <lavetisandeep03@gmail.com>
2025-03-11 10:42:01 +0530

    ff

--- src/Components/level2.jsx
+++ src/Components/level2.jsx
@@ -183,7 +183,7 @@ int main()
     // }
 
     const updateTimer = () => {
-      const startTime = new Date("2025/03/11 10:16:00");
+      const startTime = new Date("2025/03/11 10:40:00");
       const elapsedTime = Date.now() - startTime;
 
       const newTimeLeft = Math.max(
